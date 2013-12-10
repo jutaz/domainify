52b8d4db5660470b13b67d682070a4d9618f0c7c
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-12-10 13:42:19 +0200

    Added main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+module.exports = function(template, opts) {
+    if(!opts) {
+        opts = {
+            defaultTemplate: true
+        };
+    }
+    this.reserved = ["tld", "subdomainCount", "subdomains", "name", "fits"];
+    if('string' == typeof template) {
+        tmpl = template.split('.');
+        template = [];
+        for(var i in tmpl) {
+            template.push(tmpl[i]);
+        }
+    }
+    this.template = template;
+    this.useDefaultTemplate = opts.defaultTemplate;
+    for(var i in this.template) {
+        if(this.reserved.indexOf(this.template[i]) > -1) {
+            throw new Error("Template contains reserved name, which is "+this.template[i]);
+        }
+    }
+    return domain.bind(this);
+}
+
+function domain(rawDomain, callback) {
+    dom = rawDomain.split('.');
+    subdomains = dom.slice(0, dom.length-1);
+    if(this.useDefaultTemplate) {
+        dmn = {
+            tld: null,
+            subdomainCount: 0,
+            name: '',
+            subdomains: [],
+            fits: false
+        }
+        dmn.tld = dom[dom.length-1];
+        dmn.subdomainCount = subdomains.length;
+        dmn.subdomains = subdomains;
+        dmn.name = dom[dom.length-2];
+        dmn.original = rawDomain;
+    } else {
+        dmn = {};
+    }
+    if(this.template) {
+        cnt = dom.length
+        if(this.useDefaultTemplate) {
+            cnt = cnt-2;
+        }
+        for(var i = 0; i < dom.slice(0, cnt).length; i++) {
+            if(this.template[i]) {
+                dmn[this.template[i]] = (this.useDefaultTemplate) ? subdomains[i] : dom[i];
+            }
+        }
+    }
+    if(this.useDefaultTemplate && this.template.length == subdomains.length-1) {
+        dmn.fits = true;
+    }
+    callback(null, dmn);
+}
